146ce302244cb732e54a2d317549361dede2b4e6
Przemyslaw Wroblewski <przemyslaw.wroblewski@nokaut.pl>
2016-02-02 11:28:42 +0100

    Allegro::State.find_by_name method added

--- lib/allegro_api/state.rb
+++ lib/allegro_api/state.rb
@@ -8,9 +8,14 @@ module AllegroApi
               ['mazowieckie', 7], ['opolskie', 8], ['podkarpackie', 9], ['podlaskie', 10], ['pomorskie', 11], ['śląskie', 12], ['świętokrzyskie', 13],
               ['warmińsko-mazurskie', 14], ['wielkopolskie', 15], ['zachodniopomorskie', 16]]
     ID_TO_STATE_MAP = Hash[*STATES.flatten.reverse]
+    STATE_TO_ID_MAP = Hash[*STATES.flatten]
 
     def self.find_by_id(state_id)
-      new(id: state_id, name: ID_TO_STATE_MAP[state_id])
+      ID_TO_STATE_MAP[state_id] && new(id: state_id, name: ID_TO_STATE_MAP[state_id])
+    end
+    def self.find_by_name(state_name)
+      state_name = state_name.downcase
+      STATE_TO_ID_MAP[state_name] && new(id: STATE_TO_ID_MAP[state_name], name: state_name)
     end
   end
 end
--- spec/models/state_spec.rb
+++ spec/models/state_spec.rb
@@ -1,19 +1,35 @@
 require_relative '../spec_helper'
 
 describe AllegroApi::State do
-  let(:state_id) { self.class.metadata[:description].to_i }
-  subject { AllegroApi::State.find_by_id(state_id).name }
+  describe 'find_by_id' do
+    let(:state_id) { self.class.metadata[:description].to_i }
+    subject { AllegroApi::State.find_by_id(state_id).name }
 
-  describe '1' do
-    it { is_expected.to eq('dolnośląskie') }
+    describe '1' do
+      it { is_expected.to eq('dolnośląskie') }
+    end
+    describe '2' do
+      it { is_expected.to eq('kujawsko-pomorskie') }
+    end
+    describe '16' do
+      it { is_expected.to eq('zachodniopomorskie') }
+      it 'has id' do
+        expect(AllegroApi::State.find_by_id(state_id).id).to eq(state_id)
+      end
+    end
+    describe '99' do
+      subject { AllegroApi::State.find_by_id(state_id) }
+      it { is_expected.to be_nil }
+    end
   end
-  describe '2' do
-    it { is_expected.to eq('kujawsko-pomorskie') }
-  end
-  describe '16' do
-    it { is_expected.to eq('zachodniopomorskie') }
-    it 'has id' do
-      expect(AllegroApi::State.find_by_id(state_id).id).to eq(state_id)
+  describe 'find_by_name' do
+    let(:state_name) { self.class.metadata[:description] }
+    subject { AllegroApi::State.find_by_name(state_name) }
+    describe 'Kujawsko-pomorskie' do
+      it { is_expected.to have_attributes(id: 2) }
+    end
+    describe 'pacanowskie' do
+      it { is_expected.to be_nil }
     end
   end
 end
